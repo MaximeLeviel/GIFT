0b1bbbeac816344ce4fb8ee0f9b5b3cd91895b42
Maxime <maxime.leviel@efrei.net>
2023-02-21 11:29:53 +0100

    Create models

---
+++ backend/gift/src/main/java/com/st2apr/gift/models/Company.java
@@ -0,0 +1,49 @@
+package com.st2apr.gift.models;
+
+public class Company {
+    private String name;
+    private String address;
+    private String tutor;
+    private String startDate;
+    private String endDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(String tutor) {
+        this.tutor = tutor;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
