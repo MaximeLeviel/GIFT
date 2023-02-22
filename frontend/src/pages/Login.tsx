a0dfd66cb2047e94734fcd55b7c7865eec17c3f3
NicolasCrssx <nicolas.cresseaux@gmail.com>
2023-02-22 16:51:32 +0100

    Init routes

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,19 @@
+import { NotificationsProvider } from "@mantine/notifications";
+import Authentication from "./Authentication";
+import { useState } from "react";
+import Home from "./Home";
+
+export default function Login() {
+
+    const [user, setUser] = useState(null);
+    return (
+    <NotificationsProvider position={"top-right"} zIndex={3015}>
+        {user ? (
+            <Home setUser={setUser} />
+        ) : (
+            <Authentication setUser={setUser} />
+        )}
+    </NotificationsProvider>
+    );
+}
+
