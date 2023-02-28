c920f4644ccf469de8e6a4b4d3292b83a704e66a
Nastir-F <tristan.fievet@efrei.net>
2023-02-28 09:54:01 +0100

    Create student path

--- backend/gift/src/main/java/com/st2apr/gift/controller/StudentController.java
+++ backend/gift/src/main/java/com/st2apr/gift/controller/StudentController.java
@@ -3,7 +3,6 @@ package com.st2apr.gift.controller;
 import com.st2apr.gift.model.Student;
 import com.st2apr.gift.repository.StudentRepository;
 import jakarta.ws.rs.*;
-
 import java.util.List;
 
 @Path("/students")
@@ -12,20 +11,21 @@ public class StudentController {
     final StudentRepository studentRepository = new StudentRepository();
 
     @GET()
-    @Produces("text/plain")
+    @Produces("application/json")
     public List<Student> getAllStudents() {
         return studentRepository.findAllStudents();
     }
 
     @GET()
     @Path("/{id}")
-    @Produces("text/plain")
+    @Produces("application/json")
     public Student getStudentById(@PathParam("id") int id) {
         return studentRepository.findById(id);
     }
 
     @POST
-    public void createStudent() {
-        System.out.println("Student created");
+    @Consumes("application/json")
+    public void createStudent(Student student) {
+        studentRepository.create(student);
     }
 }
