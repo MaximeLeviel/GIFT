f6ae0b3ee4f33ce7fda675066c43b96c0dc6910c
NicolasCrssx <nicolas.cresseaux@gmail.com>
2023-03-03 10:58:54 +0100

    Premier changement de route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
 import { MantineProvider } from "@mantine/core";
-import Login from "./pages/Login";
 import Layout from "./Layout";
 import { Routes, Route } from "react-router-dom";
-
+import { useState } from "react";
+import Home from "./pages/Home";
+import Authentication from "./components/Authentication";
+import Login from "./pages/Login";
 
 export default function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Routes>
-        <Route path="/" element={<Layout />}> 
-          <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Layout />}>
+
+          <Route path="/login" element={<Login />} />      
+
+          <Route path="/home" element={<Home setUser={setUser}/>} />
+
         </Route>
       </Routes>
     </MantineProvider>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,15 +1,18 @@
 import { NotificationsProvider } from "@mantine/notifications";
 import Authentication from "../components/Authentication";
 import { useState } from "react";
-import Home from "./Home";
+import { Navigate } from "react-router-dom";
 
 export default function Login() {
 
     const [user, setUser] = useState(null);
+
+    console.log("user :", user)
+
     return (
     <NotificationsProvider position={"top-right"} zIndex={3015}>
         {user ? (
-            <Home setUser={setUser} />
+            <Navigate to="/home" replace />
         ) : (
             <Authentication setUser={setUser} />
         )}
